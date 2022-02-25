b4b062acb6d455fa6898495728e59d3fca323dae
LucaVerth <luca.verthuy@gmail.com>
2022-02-25 17:15:22 +0100

    seed refresh

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(MessagesTableSeeder::class);
+        $this->call(ReviewsTableSeeder::class);
+        $this->call(SpecializationsTableSeeder::class);
+        $this->call(UserSpecializationTableSeeder::class);
     }
 }
