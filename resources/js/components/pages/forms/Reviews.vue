17df59a18e99f5e87e08a4b7a13a9c31b5acec26
GiuliaSardell <giuliasardelli1234@gmail.com>
2022-03-08 12:49:55 +0100

    modifiche doctorpage

--- resources/js/components/pages/forms/Reviews.vue
+++ resources/js/components/pages/forms/Reviews.vue
@@ -1,63 +1,66 @@
 <template>
-    <form @submit="sendReview" action="http://127.0.0.1:8000/api/review/" method="POST">
-        <div class="form-row">
-            <input type="hidden" name="user_id" id="user_id" :value="doctor_id"/>
-            <div class="form-group col-md-6">
-                <label for="vote">Voto (da 1 a 5)</label>
-                <input
-                    type="number"
-                    min="1"
-                    max="5"
-                    class="form-control"
-                    id="vote"
-                    name="vote"
-                    v-model="vote"
+    <div>
+        <h5 class="my-3 font-weight-bold">Lascia una recensione al medico</h5>
+        <form @submit="sendReview" action="http://127.0.0.1:8000/api/review/" method="POST">
+            <div class="form-row">
+                <input type="hidden" name="user_id" id="user_id" :value="doctor_id"/>
+                <div class="form-group col-md-6">
+                    <label for="vote">Voto (da 1 a 5)</label>
+                    <input
+                        type="number"
+                        min="1"
+                        max="5"
+                        class="form-control"
+                        id="vote"
+                        name="vote"
+                        v-model="vote"
+                        required
+                    />
+                    <p v-if="error.vote" class="errors">{{ error.vote[0] }}</p>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="user_review_name">Nome</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        name="user_review_name"
+                        id="user_review_name"
+                        v-model="review_name"
+                    />
+                    <p v-if="error.user_review_name" class="errors">{{ error.user_review_name[0] }}</p>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="d-block" for="content">Recensione</label>
+                <textarea
+                    name="content"
+                    id="content"
+                    rows="5"
+                    v-model="content"
                     required
-                />
-                <p v-if="error.vote" class="errors">{{ error.vote[0] }}</p>
+                ></textarea>
+                <p v-if="error.content" class="errors">{{ error.content[0] }}</p>
             </div>
-            <div class="form-group col-md-6">
-                <label for="user_review_name">Nome</label>
-                <input
-                    type="text"
-                    class="form-control"
-                    name="user_review_name"
-                    id="user_review_name"
-                    v-model="review_name"
-                />
-                <p v-if="error.user_review_name" class="errors">{{ error.user_review_name[0] }}</p>
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="d-block" for="content">Recensione</label>
-            <textarea
-                name="content"
-                id="content"
-                cols="50"
-                rows="5"
-                v-model="content"
-                required
-            ></textarea>
-            <p v-if="error.content" class="errors">{{ error.content[0] }}</p>
-        </div>
-        <!-- <div class="form-group">
-            <div class="form-check">
-                <input
-                    class="form-check-input"
-                    type="checkbox"
-                    id="gridCheck"
-                />
-                <label class="form-check-label" for="gridCheck">
-                   Privacy Policy
-                </label>
-            </div>
-        </div> -->
-        <button
-            type="submit"
-            class="btn btn-primary"
-            :disabled="sending"
-        >{{ sending ? 'invio in corso' : 'invia'}}</button>
-    </form>
+            <!-- <div class="form-group">
+                <div class="form-check">
+                    <input
+                        class="form-check-input"
+                        type="checkbox"
+                        id="gridCheck"
+                    />
+                    <label class="form-check-label" for="gridCheck">
+                    Privacy Policy
+                    </label>
+                </div>
+            </div> -->
+            <button
+                type="submit"
+                class="btn btn-primary"
+                :disabled="sending"
+            >{{ sending ? 'invio in corso' : 'invia'}}</button>
+        </form>
+    </div>
+    
 </template>
 
 <script>
@@ -99,5 +102,9 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+textarea{
+    width: 100%;
+}
+</style>
 
